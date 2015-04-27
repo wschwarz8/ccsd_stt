ea0bf183c9719067b8eedc0d37814cd77d637cca
Austin Hamilton <hamiltonaustin@ccsd.k12.ia.us>
2015-04-27 15:30:47 -0400

    Database connection successful.

--- students_skills.php
+++ students_skills.php
@@ -1,19 +1,21 @@
 <?php
-mysql_connect('localhost', 'techmod', 'QbKrlIpdB');
+$g_link = mysql_connect('localhost', 'techmod', 'QbKrlIpdB');
 
 $query = "SELECT `skills`.*, `students`.*, `studentsxskills`.`stid`
 FROM `studentsxskills`, `skills`, `students`
 WHERE ((`studentsxskills`.`stid` = students.id) AND (`studentsxskills`.`skid` = skills.id))";
 
+mysql_select_db('stt', $g_link);
+	
 $result = mysql_query($query);
 if (!$result) {
     die('Invalid query: ' . mysql_error());
 }
 
 while ($row = mysql_fetch_assoc($result)) {
-    echo $row[name];
-    echo $row[gradclass];
-    echo $row[skillName];
+    echo $row["name"];
+    echo $row["class"];
+    echo $row["skillName"];
     
 }
 ?>
