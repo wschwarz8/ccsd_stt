0de7a70205733aa5d3d8116c6fcb3fd21556d4d1
Local Admin <ladmin@potassium.cps.loc>
2015-04-28 14:30:03 -0500

    Created a simple skill entryform

--- enter_skillNames.php
+++ enter_skillNames.php
@@ -3,7 +3,16 @@ if($POST)
 {
 		
 }
-else
-{
-		
+else {
+?>
+Enter a new skill<BR><BR>
+<table>
+<form name='theForm' method='POST'>
+<tr><td>Skill name</td><td><input type='text' name='skillName'></td></tr>
+<tr><td colspan=2><input type='submit'></td></tr>
+</tr>
+</form>
+</table>
+<?php
 }
+?>
