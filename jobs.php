0fc630dd000bfd850f4492ae0df648fc96e94a50
Walter Schwarz <walter.h.schwarz@gmail.com>
2015-08-31 14:18:22 -0500

    removed names for no claim

--- jobs.php
+++ jobs.php
@@ -3,7 +3,7 @@ require_once 'config.php';
 
 $g_link = mysql_connect('localhost', $g_username, $g_password); //TODO use a persistant database connections
 
-$query = "SELECT a.name, a.description, b.category, a.points, c.name
+$query = "SELECT a.name as sname, a.description, b.category, a.points, c.name
 FROM jobs a, skillcategories b, students c
 WHERE a.skillcatid=b.id AND a.status=1 AND (a.claimedby=c.id OR (a.claimedby=0 AND c.id=9))";
 
@@ -18,7 +18,11 @@ if (!$result) {
 echo "<table border=1>";
 echo "<tr><td>Job</td><td>Description</td><td>Points</td><td>Category</td><td>Claimed By</td></tr>";
 while ($row = mysql_fetch_assoc($result)) { // TODO format to look better
-    echo "<tr><td>".$row['name']."</td><td>".$row['description']."</td><td>".$row['points']."</td><td>".$row['category']."</td><td>".$row['name']."</td></tr>";
+    echo "<tr><td>".$row['name']."</td><td>".$row['description']."</td><td>".$row['points']."</td><td>".$row['category']."</td><td>";
+    if($row['name'] != 'Steavie'){
+         echo $row['name']."</td></tr>";
+    }
+    echo "-</td></tr>";
 }
 echo "</table>";
 
