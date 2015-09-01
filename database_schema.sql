6f0f207c9a43d2a43e11704324e11d2961bfce87
Walter Schwarz <walter.h.schwarz@gmail.com>
2015-09-01 10:31:19 -0500

    Updated database schema to include priority

--- database_schema.sql
+++ database_schema.sql
@@ -3,9 +3,9 @@
 -- http://www.phpmyadmin.net
 --
 -- Host: localhost
--- Generation Time: Aug 27, 2015 at 04:38 PM
--- Server version: 5.5.38-0ubuntu0.14.04.1
--- PHP Version: 5.5.9-1ubuntu4.5
+-- Generation Time: Sep 01, 2015 at 10:29 AM
+-- Server version: 5.5.44-0ubuntu0.14.04.1
+-- PHP Version: 5.5.9-1ubuntu4.11
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET time_zone = "+00:00";
@@ -19,8 +19,6 @@ SET time_zone = "+00:00";
 --
 -- Database: `stt`
 --
-CREATE DATABASE IF NOT EXISTS `stt` DEFAULT CHARACTER SET latin1 COLLATE latin1_swedish_ci;
-USE `stt`;
 
 -- --------------------------------------------------------
 
@@ -39,114 +37,11 @@ CREATE TABLE IF NOT EXISTS `jobs` (
   `repeatable` tinyint(1) NOT NULL DEFAULT '0',
   `limitone` tinyint(1) NOT NULL DEFAULT '0',
   `claimedby` int(11) NOT NULL DEFAULT '0',
+  `priority` int(11) NOT NULL DEFAULT '1',
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
-
--- --------------------------------------------------------
-
---
--- Table structure for table `jobstatus`
---
-
-DROP TABLE IF EXISTS `jobstatus`;
-CREATE TABLE IF NOT EXISTS `jobstatus` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
-  `name` varchar(32) NOT NULL,
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
-
--- --------------------------------------------------------
-
---
--- Table structure for table `points`
---
-
-DROP TABLE IF EXISTS `points`;
-CREATE TABLE IF NOT EXISTS `points` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
-  `student_id` int(11) NOT NULL,
-  `points` int(11) NOT NULL,
-  `category_id` int(11) NOT NULL,
-  `timestamp` timestamp NOT NULL DEFAULT '0000-00-00 00:00:00' ON UPDATE CURRENT_TIMESTAMP,
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
-
--- --------------------------------------------------------
-
---
--- Table structure for table `Rewards`
---
-
-DROP TABLE IF EXISTS `Rewards`;
-CREATE TABLE IF NOT EXISTS `Rewards` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
-  `title` varchar(64) NOT NULL,
-  `category` int(11) NOT NULL DEFAULT '1',
-  `points` int(11) NOT NULL,
-  `description` varchar(256) NOT NULL,
-  PRIMARY KEY (`id`),
-  UNIQUE KEY `title` (`title`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
-
--- --------------------------------------------------------
-
---
--- Table structure for table `skillcategories`
---
-
-DROP TABLE IF EXISTS `skillcategories`;
-CREATE TABLE IF NOT EXISTS `skillcategories` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
-  `category` varchar(30) NOT NULL,
-  `suit` varchar(16) DEFAULT NULL,
-  `Theme` varchar(16) DEFAULT NULL,
-  PRIMARY KEY (`id`),
-  UNIQUE KEY `category` (`category`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
-
--- --------------------------------------------------------
-
---
--- Table structure for table `skills`
---
-
-DROP TABLE IF EXISTS `skills`;
-CREATE TABLE IF NOT EXISTS `skills` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
-  `skillName` varchar(32) NOT NULL,
-  `skillcatid` int(11) NOT NULL DEFAULT '1',
-  PRIMARY KEY (`id`),
-  UNIQUE KEY `skillName` (`skillName`),
-  KEY `skillcatid` (`skillcatid`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
-
--- --------------------------------------------------------
-
---
--- Table structure for table `students`
---
-
-DROP TABLE IF EXISTS `students`;
-CREATE TABLE IF NOT EXISTS `students` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
-  `name` varchar(40) NOT NULL,
-  `class` varchar(4) NOT NULL,
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
-
--- --------------------------------------------------------
-
---
--- Table structure for table `studentsxskills`
---
-
-DROP TABLE IF EXISTS `studentsxskills`;
-CREATE TABLE IF NOT EXISTS `studentsxskills` (
-  `stid` int(11) NOT NULL,
-  `skid` int(11) NOT NULL,
-  PRIMARY KEY (`stid`,`skid`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=26 ;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+
