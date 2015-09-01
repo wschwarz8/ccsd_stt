662270c2d1372e90a7a7b03e6295deda484fa8ec
Walter Schwarz <walter.h.schwarz@gmail.com>
2015-09-01 10:28:15 -0500

    Added priority to job listing

--- jobs.php
+++ jobs.php
@@ -3,9 +3,10 @@ require_once 'config.php';
 
 $g_link = mysql_connect('localhost', $g_username, $g_password); //TODO use a persistant database connections
 
-$query = "SELECT a.name as sname, a.description, b.category, a.points, c.name
+$query = "SELECT a.name as sname, a.description, b.category, a.points, c.name, a.priority
 FROM jobs a, skillcategories b, students c
-WHERE a.skillcatid=b.id AND a.status=1 AND (a.claimedby=c.id OR (a.claimedby=0 AND c.id=9))";
+WHERE a.skillcatid=b.id AND a.status=1 AND (a.claimedby=c.id OR (a.claimedby=0 AND c.id=9))
+ORDER BY priority DESC, category";
 
 mysql_select_db('stt', $g_link);
 	
@@ -18,7 +19,13 @@ if (!$result) {
 echo "<table border=1>";
 echo "<tr><td>Job</td><td>Description</td><td>Points</td><td>Category</td><td>Claimed By</td></tr>";
 while ($row = mysql_fetch_assoc($result)) { // TODO format to look better
-    echo "<tr><td>".$row['sname']."</td><td>".$row['description']."</td><td>".$row['points']."</td><td>".$row['category']."</td><td>";
+    if($row['priority']>7)
+	echo "<tr bgcolor='red'>";
+    else if($row['priority']>3)
+	echo "<tr bgcolor='orange'>";
+    else
+	echo "<tr>";
+    echo "<td>".$row['sname']."</td><td>".$row['description']."</td><td>".$row['points']."</td><td>".$row['category']."</td><td>";
     if($row['name'] != 'Steavie'){
          echo $row['name']."</td></tr>";
     }
