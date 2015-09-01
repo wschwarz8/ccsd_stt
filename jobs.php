a6f69e20529cf87d8dedbbbfc018f721b05380fe
Walter Schwarz <walter.h.schwarz@gmail.com>
2015-09-01 11:25:34 -0500

    Made repeatable jobs unclaimable

--- jobs.php
+++ jobs.php
@@ -3,9 +3,9 @@ require_once 'config.php';
 
 $g_link = mysql_connect('localhost', $g_username, $g_password); //TODO use a persistant database connections
 
-$query = "SELECT a.name as sname, a.description, b.category, a.points, c.name, a.priority
+$query = "SELECT a.name as sname, a.description, b.category, a.points, c.name, a.priority, a.repeatable
 FROM jobs a, skillcategories b, students c
-WHERE a.skillcatid=b.id AND a.status=1 AND (a.claimedby=c.id OR (a.claimedby=0 AND c.id=9))
+WHERE status=1 AND a.skillcatid=b.id AND a.status=1 AND (a.claimedby=c.id OR (a.claimedby=0 AND c.id=9))
 ORDER BY priority DESC, category";
 
 mysql_select_db('stt', $g_link);
@@ -28,6 +28,9 @@ while ($row = mysql_fetch_assoc($result)) { // TODO format to look better
     echo "<td>".$row['sname']."</td><td>".$row['description']."</td><td>".$row['points']."</td><td>".$row['category']."</td><td>";
     if($row['name'] != 'Steavie'){
          echo $row['name']."</td></tr>";
+    }
+    else if($row['repeatable']){
+	echo "all</td></tr>";
     }
     echo "-</td></tr>";
 }
