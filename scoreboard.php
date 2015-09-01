6ed10b0d62495b623ad804c68115f3145d5ce46f
Walter Schwarz <walter.h.schwarz@gmail.com>
2015-09-01 09:05:45 -0500

    Added weekly scores

--- scoreboard.php
+++ scoreboard.php
@@ -3,9 +3,16 @@ require_once 'config.php';
 
 $g_link = mysql_connect('localhost', $g_username, $g_password); //TODO use a persistant database connections
 
+$weekscore = true;
+
 $query = "SELECT a.name, b.points, c.category
 FROM students a, points b, skillcategories c
 WHERE a.id=b.student_id AND b.category_id=c.id";
+
+if($weekscore){ // If this is a weekly score and not a total score
+    $last_sunday = date('Y-m-d H:i_s', strtotime('last Sunday'));
+    $query .= " AND b.timestamp > '$last_sunday'";
+}
 
 mysql_select_db('stt', $g_link);
 	
@@ -27,6 +34,16 @@ mysql_close($g_link);
 
 arsort($scoreboard);
 
+?>
+<html>
+<head>
+<meta http-equiv="refresh" content="60">
+</head>
+<body>
+<?php
+if($weekscore) {
+	echo "<h3>Scores for the Current Week</h3>";
+}
 echo "<table><tr><td>";
 echo "<tr><td>Student</td><td>Score</td>";
 foreach ($scoreboard as $key => $value) {
@@ -35,3 +52,5 @@ foreach ($scoreboard as $key => $value) {
 echo "</table>";
 
 ?>
+</body>
+</html>
