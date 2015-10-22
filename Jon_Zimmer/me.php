0e6767734581c3c3da7011114f024f9f5259c0fe
jzimmer18 <jzimmer18@outlook.com>
2015-10-22 12:34:11 -0400

    did php

---
+++ Jon_Zimmer/me.php
@@ -0,0 +1,18 @@
+<html>
+	<title></title>
+		<head>
+			<body>
+				<?php
+				$page = rand(0,1)
+?>
+				<script>
+				if $page=0{
+					echo "This is my boring page!!"
+				}
+				</script>
+			</body>
+	</head>
+</html><?php
+	
+	}
+?>
