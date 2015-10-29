8a3c0436eaa45ef642d7a3c841ec5b7fdedfd60a
maxdevos49 <maxdevos49@gmail.com>
2015-10-28 21:15:31 -0400

    created header and footer functions with a default style for now

---
+++ index.php
@@ -0,0 +1,23 @@
+<?php
+	require_once "functions.php";
+	makeHeader("STT HOME","STT HOME",0);
+?>
+
+<BR>
+Welcome to the site.
+<BR><BR>
+No code has been posted yet.
+
+Visit <a href="phpmyadmin">here</a> for php MyAdmin.<BR><BR>
+Visit <a href="students_skills.php">here</a> for a list of Students and Skills.<BR><BR>
+Visit <a href="enter_skillNames.php">here</a> for entering skill names.<BR><BR>
+Visit <a href="scoreboard.php">here</a> for the scoreboard.<BR><BR>
+Visit <a href="jobs.php">here</a> for the Job List.<BR><BR>
+Visit <a href="team.html">here</a> for the team page.<BR><BR>
+Visit <a href="Login%20Stuff/login.php"> here</a> for the login page.<BR><BR>
+	
+
+	
+<?php
+	makefooter("Contact Max for any questions about the Header or Footer functions at devosmaxwell@ccsd.k12.ia.us",0);
+?>
