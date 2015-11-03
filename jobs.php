477b06668395affa50ba1dccd8c47175e8a463c7
Walter Schwarz <walter.h.schwarz@gmail.com>
2015-11-03 09:12:54 -0600

    Added devices to jobs listing

--- jobs.php
+++ jobs.php
@@ -15,6 +15,31 @@ if(isset($_GET["Student"])){
 	echo "hgnrjefioesndgj";
 }
 
+function printjobs($result) {
+  while ($row = mysql_fetch_assoc($result)) { // TODO format to look better
+    if($row['priority']>7)
+	echo "<tr bgcolor='red'>";
+    else if($row['priority']>3)
+	echo "<tr bgcolor='orange'>";
+    else
+	echo "<tr>";
+    echo "<td>".$row['sname']."</td><td>".$row['description']."</td><td>".$row['points']."</td><td>".$row['category']."</td><td>";
+    if($row['name'] != 'Steavie'){
+         echo $row['name']."</td></tr>";
+    }
+    else if($row['repeatable']){
+	echo "all</td></tr>";
+    }
+    echo 
+		"<button type='button' id='button' onclick='claimjobfunction(".$row['id'].")'>
+		Claim Job
+	</button>
+	
+	</td></tr>";
+  } // End While
+}
+	
+
 $script = "
 		<script>
 	function claimjobfunction(jobid) {
@@ -46,15 +71,22 @@ makeHeader("Job List","Job List",3,$script);
 <?php
 
 $g_link = mysql_connect('localhost', $g_username, $g_password); //TODO use a persistant database connections
+mysql_select_db('stt', $g_link);
 
 $query = "SELECT a.name as sname, a.description, b.category, a.points, c.name, a.priority, a.repeatable, a.id
 FROM jobs a, skillcategories b, students c
 WHERE status<4 AND a.skillcatid=b.id AND a.status=1 AND (a.claimedby=c.id OR (a.claimedby=0 AND c.id=9))
 ORDER BY priority DESC, category";
 
-mysql_select_db('stt', $g_link);
+$query2 = "SELECT owner as sname, problem as description, 'Computer Hardware' as category, point_value as points, b.name, 1 as priority, 0 as repeatable, a.id 
+FROM devices a, students b 
+WHERE status_id < 6 AND (a.assignedto_id = b.id OR (a.assignedto_id=0 AND b.id=9))
+ORDER BY status_id";
 	
 $result = mysql_query($query);
+
+$result2 = mysql_query($query2);
+
 if (!$result) {
     die('Invalid query: ' . mysql_error());
 }
@@ -62,29 +94,8 @@ if (!$result) {
 // prints one row at a time, the results from the database.
 echo "<table border=1>";
 echo "<tr><td>Job</td><td>Description</td><td>Points</td><td>Category</td><td>Claimed By</td></tr>";
-while ($row = mysql_fetch_assoc($result)) { // TODO format to look better
-    if($row['priority']>7)
-	echo "<tr bgcolor='red'>";
-    else if($row['priority']>3)
-	echo "<tr bgcolor='orange'>";
-    else
-	echo "<tr>";
-    echo "<td>".$row['sname']."</td><td>".$row['description']."</td><td>".$row['points']."</td><td>".$row['category']."</td><td>";
-    if($row['name'] != 'Steavie'){
-         echo $row['name']."</td></tr>";
-    }
-    else if($row['repeatable']){
-	echo "all</td></tr>";
-    }
-    echo 
-		"<button type='button' id='button' onclick='claimjobfunction(".$row['id'].")'>
-		Claim Job
-	</button>
-	
-	</td></tr>";
-	
-} // end while
-
+printjobs($result2);
+printjobs($result);
 	
 echo "</table>";
 
