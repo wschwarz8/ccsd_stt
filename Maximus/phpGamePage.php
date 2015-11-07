0eb424fe5e1cc14f78e01c5b3a382ed575a8ad25
maxdevos49 <maxdevos49@gmail.com>
2015-11-06 19:39:31 -0500

    started on my html game

--- Maximus/phpGamePage.php
+++ Maximus/phpGamePage.php
@@ -1,9 +1,51 @@
 <?php
 require_once "../functions.php";
 require_once "../config.php";
-makeHeader("Game", "Game", 3);
+makeHeader("Color Mixer", "Color Mixer", 0,'<link href="css_files/phpGamePage.css" rel="stylesheet"><link href="css_files/commonCSS.css" rel="stylesheet"><link href="css_files/phpPage.css" rel="stylesheet"><script language="javascript" type="text/javascript" src="javascript_files/phpGamePage.js"></script>');
 
+?>
 
+<style>
+  div{
+    text-align:center;
+  }
 
-makeFooter("",3,"false");
+</style>
+
+  <div class="gameframe" style="background:black;opacity:0.9;margin:10px 1% 10px 1%">
+    
+    <div class="colorBox" style="background:white;height:175px;">
+      
+      <h1 id="colorBoxText" style="text-align:center; color:Black;">Color Choices</h1>
+      
+      <div class="colorChoice" style="background:black;height:105px;margin:5px 1% 5px 1%;padding-top:5px;">
+        <div class="red" draggable="true">
+          Red
+        </div>
+        <div class="Orange" draggable="true">
+          Orange
+        </div>
+        
+        <div class="Yellow" draggable="true">
+          Yellow
+        </div>
+        
+        <div class="Green" draggable="true">
+          Green
+        </div>
+        
+        <div class="Blue" draggable="true">
+          Blue
+        </div>
+        
+        <div class="Purple" draggable="true">
+          Purple
+        </div>
+      </div>
+    </div>
+  </div>
+
+  <?php
+
+makeFooter("",0,"false");
 ?>
