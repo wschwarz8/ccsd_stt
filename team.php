e7f6b104c026da72217f8415aabf5cd6c15aef43
maxdevos49 <maxdevos49@gmail.com>
2015-11-18 17:54:37 -0500

    added really cool expandable descriptions to the teams page. Go checkit out.

--- team.php
+++ team.php
@@ -1,63 +1,226 @@
 <?php
 	require_once "functions.php";
-	makeHeader("Team Pages", "Team Pages", 2,"<style>.meat{position:absolute;width:77%;background:black;color:White;margin-top:5px;border-radius:10px;padding:5px;opacity:0.9;}</style>");
+	makeHeader("Team Pages", "Team Pages", 2,"<style>.meat{position:absolute;width:50%;background:black;color:White;margin-top:3%; margin-left:15%;border-radius:10px;padding:5px;opacity:0.9;}</style>");
 ?>
-
 <div class="meat">
-	
-<BR>
-<a href='walter'>
-	<div class="personBox">
-		<h1 id="initial">W.S.</h1>
-		<h1 id="name">Walter</h1>
-		<img class="personPic" src="http://goo.gl/zBA0Gk" height="100px" width="100px">
-	</div>
-</a>
-<BR>
-<a href='Jaydon'>Jaydon</a>
-<BR>
-<a href='Mason'>Mason</a>
-<BR>
-<a href='mckayla'>McKayla</a>
-<BR>
-<a href='Daniel'>Daniel</a>
-<BR>
-<a href='britton'>Britton</a>
-<BR>
-<a href="Jon_Zimmer">Jon Zimmer</a>
-<BR>
-<a href="Hamdawg">Austin</a>
-<BR>
-<a href="Practice">Ethan</a>
-<BR>
-<a href="Maximus">Max</a>
-<BR>
-<a href="Wyatt">Wyatt</a>
-	
-</div>
 
+	<BR>
+	<section style="float:left;width:50%">
+		<center>
+			<a href='walter'>
+				<div class="personBox" id="walter">
+					<h2 id="toph2" style="background:red;">Walter</h2>
+					<ul>
+						<li>
+							<p>add a few words that describe yourself here</p>
+						</li>
+						<li><img class="personPic" src="http://goo.gl/zBA0Gk" height="100px" width="100px"></li>
+					</ul>
+				</div>
+			</a>
+
+			<BR>
+
+			<a href='Jaydon'>
+				<div class="personBox" id="Jayden">
+					<h2 id="toph2" style="background:orange;">Jayden</h2>
+					<ul>
+						<li>
+							<p>add a few words that describe yourself here</p>
+						</li>
+						<li><img class="personPic" src="http://goo.gl/zBA0Gk" height="100px" width="100px"></li>
+					</ul>
+				</div>
+			</a>
+
+			<BR>
+
+			<a href='Mason'>
+				<div class="personBox" id="Mason">
+					<h2 id="toph2" style="background:yellow;">Mason</h2>
+					<ul>
+						<li>
+							<p>add a few words that describe yourself here</p>
+						</li>
+						<li><img class="personPic" src="http://goo.gl/zBA0Gk" height="100px" width="100px"></li>
+					</ul>
+				</div>
+			</a>
+
+			<BR>
+
+			<a href='mckayla'>
+				<div class="personBox" id="Mckayla">
+					<h2 id="toph2" style="background:green;">Mckayla</h2>
+					<ul>
+						<li>
+							<p>add a few words that describe yourself here</p>
+						</li>
+						<li><img class="personPic" src="http://goo.gl/zBA0Gk" height="100px" width="100px"></li>
+					</ul>
+				</div>
+			</a>
+
+			<BR>
+
+			<a href='Daniel'>
+				<div class="personBox" id="Daniel">
+					<h2 id="toph2" style="background:blue;">Daniel</h2>
+					<ul>
+						<li>
+							<p>add a few words that describe yourself here</p>
+						</li>
+						<li><img class="personPic" src="http://goo.gl/zBA0Gk" height="100px" width="100px"></li>
+					</ul>
+				</div>
+			</a>
+
+			<BR>
+
+			<a href='britton'>
+				<div class="personBox" id="Britton">
+					<h2 id="toph2" style="background:purple;">Britton</h2>
+					<ul>
+						<li>
+							<p>add a few words that describe yourself here</p>
+						</li>
+						<li><img class="personPic" src="http://goo.gl/zBA0Gk" height="100px" width="100px"></li>
+					</ul>
+				</div>
+			</a>
+
+			<BR>
+		</center>
+	</section>
+	<center>
+		<section style="float:right;width:50%">
+			<a href="Jon_Zimmer">
+				<div class="personBox" id="Jon">
+					<h2 id="toph2" style="background:red;">Jon</h2>
+					<ul>
+						<li>
+							<p>add a few words that describe yourself here</p>
+						</li>
+						<li><img class="personPic" src="http://goo.gl/zBA0Gk" height="100px" width="100px"></li>
+					</ul>
+				</div>
+			</a>
+
+			<BR>
+
+			<a href="Hamdawg">
+				<div class="personBox" id="Austin">
+					<h2 id="toph2" style="background:orange;">Austin</h2>
+					<ul>
+						<li>
+							<p>add a few words that describe yourself here</p>
+						</li>
+						<li><img class="personPic" src="http://goo.gl/zBA0Gk" height="100px" width="100px"></li>
+					</ul>
+				</div>
+			</a>
+
+			<BR>
+
+			<a href="Practice">
+				<div class="personBox" id="Ethan">
+					<h2 id="toph2" style="background:Yellow;">Ethan</h2>
+					<ul>
+						<li>
+							<p>add a few words that describe yourself here</p>
+						</li>
+						<li><img class="personPic" src="http://goo.gl/zBA0Gk" height="100px" width="100px"></li>
+					</ul>
+				</div>
+			</a>
+
+			<BR>
+
+			<a href="Maximus">
+				<div class="personBox" id="Max">
+					<h2 id="toph2" style="background:green;">Max</h2>
+					<ul>
+						<li>
+							<p>I like to code and eat non school food.</p>
+						</li>
+						<li><img class="personPic" src="http://goo.gl/G2cTc1" height="100px" width="100px"></li>
+					</ul>
+				</div>
+			</a>
+
+			<BR>
+
+			<a href="Wyatt">
+				<div class="personBox" id="Wyatt">
+					<h2 id="toph2" style="background:blue;">Wyatt</h2>
+					<ul>
+						<li>
+							<p>add a few words that describe yourself here</p>
+						</li>
+						<li><img class="personPic" src="http://goo.gl/zBA0Gk" height="100px" width="100px"></li>
+					</ul>
+				</div>
+			</a>
+				
+			<BR>
+				
+				<a href="#">
+				<div class="personBox" id="Steavie">
+					<h2 id="toph2" style="background:purple;">Steavie</h2>
+					<ul>
+						<li>
+							<p>Im just here to make the columns even :D</p>
+						</li>
+						<li><img class="personPic" src="http://goo.gl/zBA0Gk" height="100px" width="100px"></li>
+					</ul>
+				</div>
+			</a>
+				
+				<BR>
+	</center>
+	</section>
+</div>
+				</center>
 	<style>
-	
-		.personBox{
-			background:white;
+		* {
+			margin:0;
+			padding:0;
+		}
+	.personBox {
+ 		background: #fff;
+  	width: 200px; 
+		border-radius:3px;
+		text-align:center;
+		display:block;
+}
+		.personBox h2{
+ 			color: #fff;
+  		font-weight: 500;
+  		padding: 7px 12px;
+			border-radius:3px;
+		}
+		.toph2{
+			border-radius:3px;
+		}
+		a{
+  		text-decoration: none;
+		}
+		p{
 			color:black;
-			border-radius:5px;
-			width:70px;
-			height:60x;
+		}
+		.personBox ul{
+			list-style-type:none;
+			overflow:hidden;
+			padding:0;
+			height:0px;
 			
+			-webkit-transition: height 1s ease;
 		}
-		#name{
-			display:none;
-		}
-		.personPic{
-			display:none;
-		}
-		.personBox:hover{
-			width:200px;
-			height:200px;
+		.personBox:hover ul{
+			height:150px;
 		}
 	</style>
 	
 <?php
-	makeFooter("this is going to be cool", 2);
+	makeFooter("New name buttons with expandable descriptions!!!! To change your description(make this short) go into team.php and edit the p element. images can be added if they are square and urls are shortened at <a href='https://goo.gl/'>here</a>.", 2);
 ?>
