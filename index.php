87a2768fedf7e30e97235599c22c4fde1042549f
maxdevos49 <maxdevos49@gmail.com>
2015-11-25 00:23:32 -0500

    started to set up the hoe page for a news feed about the class

--- index.php
+++ index.php
@@ -1,28 +1,43 @@
 <?php
 	require_once "functions.php";
-	makeHeader("STT HOME","Student Technology Team Home",2);
+	makeHeader("STT HOME","Student Technology Team Home",2,'<link href="/css_files/homePage.css" rel="stylesheet";>');
 ?>
+	<nav>
+		<center>
+			<h2>Welcome to the site.</h2>
+			<p>
+				Visit <a href="phpmyadmin">here</a> for php MyAdmin.
+			</p>
+			<p>
+				Visit <a href="students_skills.php">here</a> for a list of Students and Skills.
+			</p>
+			<p>
+				Visit <a href="enter_skillNames.php">here</a> for entering skill names.
+			</p>
+			<p>
+				Visit <a href="scoreboard.php">here</a> for the scoreboard.
+			</p>
+			<p>
+				Visit <a href="jobs.php">here</a> for the Job List.
+			</p>
+			<p>
+				Visit <a href="team.php">here</a> for the team page.
+			</p>
+			<p>
+				Visit <a href="DisplayBroken.php"> here</a> for a list of broken chromebooks.
+			</p>
+			<p>
+				Visit <a href="login/login.php"> here</a> for the login page.
+			</p>
+		</center>
+	</nav>
+	<section class="newsDisplay">
+		<div>
+			News display here soon
+		</div>
+	</section>
 
-		<BR> Welcome to the site.
-		<BR>
-		<BR> No code has been posted yet. Visit <a href="phpmyadmin">here</a> for php MyAdmin.
-		<BR>
-		<BR> Visit <a href="students_skills.php">here</a> for a list of Students and Skills.
-		<BR>
-		<BR> Visit <a href="enter_skillNames.php">here</a> for entering skill names.
-		<BR>
-		<BR> Visit <a href="scoreboard.php">here</a> for the scoreboard.
-		<BR>
-		<BR> Visit <a href="jobs.php">here</a> for the Job List.
-		<BR>
-		<BR> Visit <a href="team.php">here</a> for the team page.
-		<BR>
-		<BR> Visit <a href="login/login.php"> here</a> for the login page.
-		<BR>
-		<BR> Visit <a href="DisplayBroken.php"> here</a> for a list of broken chromebooks.
-		<BR>
-		<BR>
 
 	<?php
-	makefooter("",2);
+	makefooter("",0,"false");
 ?>
