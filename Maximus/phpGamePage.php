fd2b83a97e5466c5248b9e21b1d4a0e74afe48d0
maxdevos49 <maxdevos49@gmail.com>
2015-11-24 23:45:19 -0500

    organized some files

--- Maximus/phpGamePage.php
+++ Maximus/phpGamePage.php
@@ -1,6 +1,5 @@
 <?php
 require_once "../functions.php";
-require_once "../config.php";
 makeHeader("Color Mixer", "Color Mixer", 2);
 ?>
 <canvas id="mainSheet" height="400px" width="600px">get a htm5 compatible browser gosh dang it!!!</canvas>
