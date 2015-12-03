b0b99e344c94ffe952574b1c5b14bc4a6cd3ce4a
Walter Schwarz <walter.h.schwarz@gmail.com>
2015-12-03 13:28:01 -0600

    added the pageviews table

--- database_schema.sql
+++ database_schema.sql
@@ -25,6 +25,19 @@ USE `stt`;
 -- --------------------------------------------------------
 
 --
+-- Table structure for table `pageviews`
+--
+
+CREATE TABLE IF NOT EXISTS `pageviews` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `filename` varchar(32) DEFAULT NULL,
+  `views` int(11) NOT NULL DEFAULT '0',
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
+
+-- --------------------------------------------------------
+
+--
 -- Table structure for table `news`
 --
 
