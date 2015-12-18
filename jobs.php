70a6ea24c1498f0b6d6cddf9f79a1c825d503efd
jzimmer18 <jzimmer18@outlook.com>
2015-12-18 14:10:16 -0500

    now I am done

--- jobs.php
+++ jobs.php
@@ -81,24 +81,26 @@ mysql_select_db('stt', $g_link);
 	$query = "SELECT a.name as sname, a.description, b.category, a.points, c.name, a.priority, a.repeatable, a.id
 	FROM jobs a, skillcategories b, students c
 	WHERE status<4 AND a.skillcatid=b.id AND a.status=1 AND (a.claimedby=c.id OR (a.claimedby=0 AND c.id=9))";
-	if ($_GET['sortby']=='category') {///order by category 
+if ($_GET['sortby']=='category') {///order by category 
 	$query = $query ." ORDER BY category";
 }
 else if ($_GET['sortby']=='points') {
-$query= $query ."order by points DESC"; 
-}
-
-
+	$query= $query ."order by points";
+}	
 else {  /// order by default 
-
-$query = $query . " ORDER BY priority DESC, points";
+	$query = $query . " ORDER BY priority DESC, points";
 }	
 	
+if ($_GET['order']=='DESC'){ 
+	$query= $query ." DESC";		 
+}
+
 	
 	
 print_r($_GET);	
 		echo $_GET['sortby'];
-	
+print_r($_GET);
+	echo $_GET['order'];
 	
 	
 	
@@ -118,10 +120,24 @@ if (!$result) {
 
 // prints one row at a time, the results from the database.
 echo "<table border=1>";
-
+if ($_GET['order']=='ASC') {
 echo "<tr><td>Job</td><td>Description</td><td>
-<a   href='jobs.php?sortby=points'>Points</a></td><td>
+<a  href='jobs.php?sortby=points&order=DESC'>Points</a> v</td><td>
 <a href='jobs.php?sortby=category'>Category</a></td><td>Claimed By</td></tr>";
+}
+		
+else if ($_GET['order']=='DESC') {
+echo "<tr><td>Job</td><td>Description</td><td>
+<a  href='jobs.php?sortby=points&order=ASC'>Points</a> ^</td><td>
+<a href='jobs.php?sortby=category'>Category</a></td><td>Claimed By</td></tr>";
+}	
+		else {
+			echo "<tr><td>Job</td><td>Description</td><td>
+<a  href='jobs.php?sortby=points&order=ASC'>Points</a></td><td>
+<a href='jobs.php?sortby=category'>Category</a></td><td>Claimed By</td></tr>";
+}	
+		
+		
 ///printjobs($result2, false);
 
 ///echo "<tr><td>Job</td><td>Description</td><td>Points</td><td>Category</td><td>Claimed By</td></tr>";
