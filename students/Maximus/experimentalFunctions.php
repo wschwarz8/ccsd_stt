b663811042f39820c3b7ad08107a79402519338e
maxdevos49 <maxdevos49@gmail.com>
2016-01-13 00:47:06 -0500

    started oop homework

---
+++ students/Maximus/experimentalFunctions.php
@@ -0,0 +1,142 @@
+<?php
+//header function
+function makeHeader($tbtitle,$ptitle,$pstyle,$fileName, $hhtml=""){
+	if(!$ptitle)$ptitle=$tbtitle;	
+	echo("
+	<!--max devos made this -->
+	<html>
+		<head>
+			<title>". $tbtitle . "</title>
+			". styleChoice($pstyle)
+			);
+	echo $hhtml;
+	echo("
+		</head>
+		<body onload='calculateStuff();'>
+			<div class='header'>
+				<a href='../index.php'><h1 id='headerTitle'>". $ptitle . "</h1></a>
+			
+				");
+		
+		//echo viewsofpage($fileName);
+	
+		echo("
+		
+				<div class='buttonRow'>
+					<a href='../index.php'><div class='buttons'>Home</div></a>
+					<a href='../jobs.php?sortby=points&order=ASC'><div class='buttons'>Jobs</div></a>
+					<a href='../scoreboard.php?type=all'><div class='buttons'>Scoreboard</div></a>
+					<a href='../team.php'><div class='buttons'>Team Pages</div></a>
+					<a href='../DisplayBroken.php'><div class='buttons'>Broken</div></a>
+					<a href='../login/login.php?logout=1'><div class='buttons'>Log Out</div></a> 
+				</div>
+		
+			<div class='meat'>
+	");
+}
+//
+//footer function to set up the end of a webpage easier
+//
+function makeFooter($footermessage,$pstyle,$footerbox=""){
+	echo("
+	<!--max devos made this -->
+	</div>
+		<style>". styleChoice($pstyle) ."</style>
+			");
+			 if ($footerbox == "false"){
+			 }else{
+				echo ("
+				<div id='footer'>
+					<p id='footerMessage'>". $footermessage . "</p>
+				</div>
+				");
+			 }
+			 echo ("
+		<body>
+	<html>
+	<script>
+	
+	function calculateStuff(){
+	
+		var body = document.body, html = document.documentElement;
+		
+		var footer = document.getElementById('footer');
+
+		var height = Math.max( body.scrollHeight, body.offsetHeight, html.clientHeight, html.scrollHeight, html.offsetHeight ) + 25;
+		
+		body.style.height = height;
+		
+		footer.style.width = '78%';
+		footer.style.height= '25px';
+		footer.style.top = height - 25;
+	}
+	
+	</script>
+	");
+}
+
+
+//stores desired theme into a variable
+function styleChoice($styleNum){
+	switch($styleNum){
+		case 1://basic style with some bare structure
+		return $styleText = '<link href="/css_files/headerStyle1.css" rel="stylesheet">';                     
+		break;
+		case 2://coding theme
+		return $styleText = '<link href="/css_files/headerStyle2.css" rel="stylesheet">';
+		break;
+		case 3://braves theme
+		return $styleText = '<link href="/css_files/headerStyle3.css" rel="stylesheet";>';
+		break;
+		default://select this style if you want to have your own style. Just have your <style> or <link> tags be your first line of code between the header and footer functions
+		return $styleText = "";
+	}
+}
+
+
+function viewsofpage($uncletony){
+	
+	//init pass
+	global $g_username,$g_password;
+	
+	//create conection
+	$g_link = mysql_connect('localhost', $g_username, $g_password);
+
+	//select database
+	mysql_select_db('stt', $g_link);
+	
+	//make query
+	$query = "SELECT views FROM `pageviews` WHERE filename='$uncletony'";
+	
+	//start and store query
+	$result = mysql_query($query);
+	
+	//store into an array
+	($result = mysql_fetch_assoc($result));
+	
+	//get variable ready for manipulation
+	$variable = ($result['views'] + 1);
+		
+	//check if their are views already or not
+		if($variable>1){
+				//add a view to the page table
+						$query = "UPDATE `pageviews` SET `views`=" . ($variable) . " WHERE `filename`= '$uncletony'";
+						$result = mysql_query($query);
+		}
+		else{
+			//add a table for the file
+				$query = "INSERT INTO `pageviews`(`filename`, `views`) VALUES ('$uncletony',1)";
+				$result = mysql_query($query);
+		}
+	
+	//print views to screen
+		echo ("
+		<div class='viewCount'>
+			<div class='viewBox'>
+				<h3 class='countLeft'>Views: </h3>
+				<h3 class='countRight'> ". $variable ." </h3>
+			</div>
+		</div>
+		");
+}
+?>
--- students/Maximus/phpPage.php
+++ students/Maximus/phpPage.php
@@ -1,33 +1,74 @@
 <?php  
-require_once '.../config.php';
-require_once '.../functions.php';
+require_once 'experimentalFunctions.php';
 
-makeHeader("PHP Page", "My PHP Page", 2);
+makeHeader("PHP Object-oriented Programing Practice", "PHP Object-oriented Programing Practice", 2, "phpPage.php");
 ?>
 
 
 		<div class= "bodyarea" id="theme1">
 			<div class="phpbox" id="theme2">
 				<h1>
-					PHP Query Test 1
+					PHP OOP Practice 1
 				</h1>
 				<div class="phpcodebox" id="theme1">
 					<?php
-						echo "test 1.2.3.4...";
-					
-						// Create connection
-						$conn = new mysqli("localhost", $g_username, $g_password);
+					class student{
 						
-						// Check connection
-						if ($conn->connect_error) {
- 						   die("Connection failed: " . $conn->connect_error);
-						} 
-						echo "Connected successfully";
-					
+						public $firstName;
+						public $lastName;
+						public $phoneNumber;
+						public $lockerNumber;
+						public $grade;
+						public $classs;
+						public $favoriteClass;
 						
+						public function test(){
+							echo "Testing 1.2.3.4... can you hear me????<br><br>";
+						}
+						
+						function __construct($f,$l,$p,$n,$g,$c,$fc){
+							$this->firstName = $f;
+							$this->lastName = $l;
+							$this->phoneNumber = $p;
+							$this->lockerNumber = $n;
+							$this->grade = $g;
+							$this->classs = $c;
+							$this->favoriteClass = $fc;
+						}
+						
+						function printall(){
+							echo "Student Data<br><br>";
+							echo "Firstname: " . $this->firstName . "<br>";
+							echo "Lastname: " . $this->lastName . "<br>";
+							echo "Phone Number:  " . $this->phoneNumber . "<br>";
+							echo "Locker Number: " . $this->lockerNumber . "<br>";
+							echo "Grade: " . $this->grade . "<br>";
+							echo "Class: " . $this->classs . "<br>";
+							echo "Favorite Class: " . $this->favoriteClass . "<br><br>";
+						}
+						
+						function __destruct(){
+							$this->firstName;
+							$this->lastName;
+							$this->phoneNumber;
+							$this->lockerNumber;
+							$this->grade;
+							$this->classs;
+							$this->favoriteClass;
+						}
+							
+					}
 					
-
-						$conn->close();
+					
+					$cat = new student("Max","DeVos","867-5306",348,11,2017,"Chemistry");
+					$cat->test();
+					$cat->printall();
+					
+					$dog = new student("Mad","Max","5306 - 867",384,11,1720,"Study hall");
+					$dog->printall();
+					
+					
+					
 					?>
 				</div>
 			</div>	
@@ -35,5 +76,5 @@ makeHeader("PHP Page", "My PHP Page", 2);
 
 
 <?php
-makeFooter("",0,"false");
+makeFooter("Programing is Fun",0,"True");
 ?>
