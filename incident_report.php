640f1697f654251511fbcc2335df1209bcba8a96
Walter Schwarz <walter.h.schwarz@gmail.com>
2016-01-21 11:54:55 -0600

    removed debug statement

--- incident_report.php
+++ incident_report.php
@@ -2,7 +2,7 @@
 	require_once "config.php";
 	require_once "functions.php";
 	promptLogin();
-//print_r($_POST);die;
+
 	makeHeader("Incident Report", "Make an Incident Report", 2,"incident_report.php", '<link href="/css_files/create_jobs.css" rel="stylesheet">')
 ?>
 
