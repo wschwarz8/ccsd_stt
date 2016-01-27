3f10a24f441603acd9e8a55d04505634c6bf54cd
Walter Schwarz <walter.h.schwarz@gmail.com>
2016-01-27 11:18:00 -0600

    Added the filed for job requirements

--- database_schema.sql
+++ database_schema.sql
@@ -179,6 +179,7 @@ CREATE TABLE IF NOT EXISTS `jobs` (
   `claimedby` int(11) NOT NULL DEFAULT '0',
   `priority` int(11) NOT NULL DEFAULT '1',
   `bypassLimit` tinyint(1) NOT NULL DEFAULT '0',
+  `requirement_id` int(11) NOT NULL DEFAULT '0',
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=31 ;
 
