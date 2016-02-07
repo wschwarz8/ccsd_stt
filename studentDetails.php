2a2538c771eefe9d6d6a2ec34584234c64073a55
jzimmer18 <jzimmer18@outlook.com>
2016-02-06 20:06:10 -0500

    Almost done, just a few bugs

--- studentDetails.php
+++ studentDetails.php
@@ -12,38 +12,35 @@ require_once "functions.php";
 if(isset($_GET['id'])){
 $g_link = mysql_connect('localhost', $g_username, $g_password); //TODO use a persistant database connections
 $stid=$_GET['id'];
-$query = "SELECT students.*
-FROM students
-WHERE id=$stid AND active=1";
-	$query2="SELECT points FROM points";
-	
+$query = "SELECT students . * , points.points
+FROM students, points
+WHERE students.id =$stid
+AND active =1";	
 if(isset($stid)) {
 	mysql_select_db('stt', $g_link);
 	$result = mysql_query($query);
 	if (!$result) {
     die('Invalid query: ' . mysql_error());
 	}
-
 	$row = mysql_fetch_assoc($result);
+	while ($row = mysql_fetch_assoc($result)){
+		$points= $row['points']+$row['points'];
+	}
 	echo "<h3>Details for ".$row['name']."</h3>";
 	echo "<table>";
 	echo "<tr><td>Class of ".$row['class']."</td>";
-	echo "<tr><td>Points that ".$row['name'] . " has: ";
-
+//	echo "<tr><td>Points that ".$row['name'] . " has: ".$points. "</td>";
+	echo "<tr><td>Points that ".$row['name'] . " has: ".$points. "</td>";
 	}
-			mysql_select_db('stt', $g_link);
-	$result2 = mysql_query($query2);
-	if (!$result2) {
-    die('Invalid query: ' . mysql_error());
-$row2=mysql_fetch_assoc($result2);	
-echo	$row2.['points']. "</td>";
+	
+	
 	}
+else{
+   echo "You forgot to tell me which student to show.<BR><BR><img src='http://storage.googleapis.com/chromeos-mgmt/0gjdgxs0jovce3/ChromeOsWallpaper/f5aac6c6-025d-4cc9-b8f5-da49114ca310'>";
+}
 	mysql_close($g_link);
 	echo "</tr>";
 	echo "</table>";
 	echo "</body></html>";
-}
-else{
-   echo "You forgot to tell me which student to show.<BR><BR><img src='http://storage.googleapis.com/chromeos-mgmt/0gjdgxs0jovce3/ChromeOsWallpaper/f5aac6c6-025d-4cc9-b8f5-da49114ca310'>";
-}
+
 ?>
