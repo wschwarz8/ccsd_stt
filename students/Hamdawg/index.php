6c0bf8353689ec0528c155e5f90a0891d78882dd
hamiltonaustin <hamiltonaustin@ccsd.k12.ia.us>
2016-03-11 09:39:00 -0500

    Adding changes to my page

--- students/Hamdawg/index.php
+++ students/Hamdawg/index.php
@@ -1,10 +1,63 @@
 <html>
 	<head>
+		<style>
+				.boxed
+		{
+			width: 800px;
+			opacity: .8;
+			border: groove black;
+			border-width: 5px;
+			font:black;
+			background-color:white;
+			border-radius: 15px;
+		}
+			.Column 
+			{
+			opacity: .8;
+			border: groove black;
+			border-width: 5px;
+			font:black;
+			background-color:white;
+			border-radius: 15px;
+			-webkit-column-count: 3; /* Chrome, Safari, Opera */
+    	-moz-column-count: 3; /* Firefox */
+    	column-count: 3;
+			}
+		</style>
+		<center>
+			<div class="boxed">
+		<h1 style= color:black>
+			Hello Boys and Girls, welcome to my webpage
+		</h1>
+			</div>
+	</center>
 		<a href="Building_Rockets.php"> Here is my page for creating a Rocketship using OOP PHP;
 		</a>
 		<SCRIPT LANGUAGE="JavaScript" TYPE="text/javascript" SRC="script.js"></SCRIPT>
 		
 	</head>
+			<div class="Column">
+	<body>
+		<h2>
+			Who Am I?
+		</h2>
+		<p>
+			My name is Austin Hamilton, I am a Senior in Highschool attending Cherokee Washington Highschool.
+		</p>
+			
 
+	</body>
+						</div>
+	<style>
+
+		html { 
+  background: url("https://wallpaperscraft.com/image/streams_steam_lava_volcano_eruption_55908_3840x2400.jpg") no-repeat center center fixed; 
+  -webkit-background-size: cover;
+  -moz-background-size: cover;
+  -o-background-size: cover;
+  background-size: cover;
+}
+
+	</style>
 			 
 </html>
