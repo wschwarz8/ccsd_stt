81c24ce601260d1cbdcea6df82bd866c2a4f6f6e
hamiltondylan <hamiltondylan@ccsd.k12.ia.us>
2016-03-15 09:28:45 -0400

    Changed my student page

---
+++ students/Dylan/index.php
@@ -0,0 +1,50 @@
+<html>
+	<center>
+  <h1 style=color:black>
+    THIS IS ME DYLAN THE HAMILTON
+  </h1>
+
+	<div class="xavier"div>
+		
+			 <h3 style=color:black>
+				 <font size="7"</font>
+				 WHO AM I
+			 </h3>
+		
+		<b5  style=color:black>
+			<font size="5"</font>
+				I am a freshmen at Washington High School in Cherokee Iowa. Outside of school I enjoy plaing football, golf, and robotics
+			Another thing I enjoy doing is coding and playing games. For example the game kingdom of loathing as shown in my background is 
+			a game I play which was made by my teacher. In the game I am a turtle tamer which is a class where you tame turtles and can even
+			throw the turtles at your enemy. In my future life I want to become a chemical engineer. At home I enjoy relaxing and playing with 
+			my three cats named Nala, Coco, and Cow
+		</b5>
+	</div>
+	</center>
+	<img src="http://wcacs.net/wp-content/uploads/chemistry.jpg" style="width:304px;height:220px;">
+	<style>
+	
+		.xavier
+	{
+			width: 800px;
+			opacity: .8;
+			border: groove light blue;
+			border-width: 5px;
+			font:black;
+			background-color:light blue ;
+			border-radius: 15px;
+		}
+	</style>
+  <style>
+	
+
+		html { 
+  background: url(http://dreamatico.com/data_images/ocean/ocean-5.jpg) no-repeat center center fixed; 
+  -webkit-background-size: cover;
+  -moz-background-size: cover;
+  -o-background-size: cover;
+  background-size: cover;
+}
+
+	</style>
+		</html>
