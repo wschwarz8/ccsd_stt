eeddcc21229bf2dd6da97b0015852c69db9672e2
jzimmer18 <jzimmer18@outlook.com>
2016-03-15 13:24:50 -0400

    Getting closer

--- students_skills.php
+++ students_skills.php
@@ -2,28 +2,39 @@
 require_once 'config.php';
 require_once "functions.php";
 
-promptLogin();
+    $g_link = mysql_connect('localhost', $g_username, $g_password); 
+		mysql_select_db('stt', $g_link);//TODO use a persistant database connections
+		$query = "SELECT * FROM `students` WHERE `active` = 1";
+		$result = mysql_query($query);
+		$row = mysql_fetch_assoc($result);
+		mysql_select_db('stt', $g_link);
+	$blank="  ";
 
-$g_link = mysql_connect('localhost', $g_username, $g_password); //TODO use a persistant database connections
+while($row = mysql_fetch_assoc($result))
+        {
+					$myid = $row['id'];
+					$myname = $row['username'];
+					$name = $row['name'];
+					$active = $row['active'];		
+					$idrow[$myid] = $myid;
+					$namerow[$myid] = $name;
+					$userrow[$myid] = $myname;
+					$activerow[$myid] = $active;
+        }
 
-$query = "SELECT `skills`.*, `students`.*, `studentsxskills`.`stid`
-FROM `studentsxskills`, `skills`, `students`
-WHERE ((`studentsxskills`.`stid` = students.id) AND (`studentsxskills`.`skid` = skills.id))";
-
-mysql_select_db('stt', $g_link);
 	
 $result = mysql_query($query);
 if (!$result) {
     die('Invalid query: ' . mysql_error());
 }
 
-// prints one row at a time, the results from the database.
-while ($row = mysql_fetch_assoc($result)) { // TODO format to look better
-    echo $row["name"];
-    echo $row["class"];
-    echo $row["skillName"];
+				echo "<table border=1>";
+				echo"<td></td>";
+		foreach($idrow as $myid){
+				echo "<td>$namerow[$myid]</td>";
+		}
+
     
-}
 
 mysql_close($g_link);
 ?>
