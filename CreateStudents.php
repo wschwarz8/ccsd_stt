42d290dd758d0e97d40b6e056aa4b7717aece169
jzimmer18 <jzimmer18@outlook.com>
2016-03-15 19:31:33 -0400

    Only one error found

--- CreateStudents.php
+++ CreateStudents.php
@@ -2,15 +2,13 @@
   require_once "functions.php";
 	require_once "config.php";
 promptLogin(1);
-
-	makeHeader("Create Jobs","Create a job",2,"create_jobs.php",'<link href="/css_files/create_jobs.css" rel="stylesheet">');
+	makeHeader("Create Students","Create a Student",2,"CreateStudents.php",'<link href="/css_files/create_jobs.css" rel="stylesheet">');
     $g_link = mysql_connect('localhost', $g_username, $g_password); 
 		mysql_select_db('stt', $g_link);//TODO use a persistant database connections
 		$query = "SELECT id FROM `students`";
 		$result = mysql_query($query);
 		$row = mysql_fetch_assoc($result);
 		mysql_select_db('stt', $g_link);
-
 ?>
 
 <center>
@@ -37,17 +35,25 @@ promptLogin(1);
     </table>
   </form>
 		<?php
-
 	 if ($_POST){
 		$Name=mysql_real_escape_string($_POST['Name']);
 		 $active="1";
 		 $id=$row['id']+"1";
 		 $admin="0";
 		 $bio=" ";
+		 $password=$_POST['Password'];
+		 $username=$_POST['Username'];
+		 $GradYear=$_POST['GraduationYear'];
 		 //make query
 $CreateStudent = "INSERT INTO `students`( `id`, `username`, `password`, `name`, `class`, `active`, `admin`, `bio`) VALUES 
-('".$id."','".$_POST['Username']."', ".$_POST['Password'].", ".$Name.", ".$_POST['GraduationYear'].", ".$active.", ".$admin.", ".$bio.")";
-
+($id,
+$username,
+$password,
+$Name,
+$GradYear,
+$active,
+$admin,
+$bio.)";         //HERE IS THE PROBLEM, FIX LATER
 		 //commence query
 		 $rsp = mysql_query($CreateStudent);
 		 
