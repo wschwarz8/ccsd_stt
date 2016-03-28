7d7ec29d6dfb7c211af454ad66fe138c8a1e324b
hamiltondylan <hamiltondylan@ccsd.k12.ia.us>
2016-03-28 09:58:46 -0400

    Changed things on my website

--- students/Dylan/index.php
+++ students/Dylan/index.php
@@ -13,11 +13,26 @@
 		
 		<b5  style=color:black>
 			<font size="5"</font>
-				I am a freshmen at Washington High School in Cherokee Iowa. Outside of school I enjoy plaing football, golf, and robotics
-			Another thing I enjoy doing is coding and playing games. For example the game kingdom of loathing as shown in my background is 
-			a game I play which was made by my teacher. In the game I am a turtle tamer which is a class where you tame turtles and can even
-			throw the turtles at your enemy. In my future life I want to become a chemical engineer. At home I enjoy relaxing and playing with 
-			my three cats named Nala, Coco, and Cow
+				I am a freshman at Washington High School in Cherokee Iowa. Outside of school I enjoy plaing football, golf, and robotics
+			Another thing I enjoy doing is coding and playing games. For example the game kingdom of loathing is 	a game I play which was made
+			by my teacher. In the game I am a turtle tamer which is a class where you tame turtles and can even	throw the turtles at your enemy.
+			In my future life I want to become a chemical engineer. At home I enjoy relaxing and playing with my three cats named Nala, Coco, and Cow
+		</b5>
+	</div>
+		<div class="Thunder"div>
+		
+			 <h3 style=color:black>
+				 <font size="7"</font>
+				 Other infomration
+			 </h3>
+		
+		<b5  style=color:black>
+			<font size="5"</font>
+			The way I got into the student tech team was mainly because of my brother and the how they were talking
+			about it during ourrobotics practices. Another interesting fact about me is that my family takes trips 
+			every summer in the past years we have gone to the Grand Canyon Yellowstone and all up the east coast.
+			But this year is going to be our biggest trip yet. We are going to go to Greece. 
+		
 		</b5>
 	</div>
 	</center>
@@ -28,12 +43,22 @@
 	{
 			width: 800px;
 			opacity: .8;
-			border: groove light blue;
+			border: groove lightblue;
 			border-width: 5px;
 			font:black;
-			background-color:light blue ;
-			border-radius: 15px;
-		}
+			background-color:lightblue ;
+			border-radius: 15px; 
+		} 
+		.thunder
+		{
+			width: 800px;
+			opacity: .8;
+			border: groove lightblue;
+			border-width: 5px;
+			font:black;
+			background-color:lightblue ;
+			border-radius: 15px; 
+		} 
 	</style>
   <style>
 	
