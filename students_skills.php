227f48dd796b32cfa3ba13613a675a4cb76b3560
Walter Schwarz <walter.h.schwarz@gmail.com>
2016-04-05 14:57:40 -0500

    made it work

--- students_skills.php
+++ students_skills.php
@@ -4,7 +4,7 @@ require_once 'config.php';
 require_once "functions.php";
     $g_link = mysql_connect('localhost', $g_username, $g_password); 
 		mysql_select_db('stt', $g_link);//TODO use a persistant database connections
-		$query = "SELECT a . * , b . * , c . * 
+		$query = "SELECT a.name , c.skillName
 FROM students a, studentsxskills b, skills c
 WHERE a.active =1
 AND a.id = b.stid
@@ -12,32 +12,18 @@ AND c.id = b.skid";
 		$result = mysql_query($query);
 		$row = mysql_fetch_assoc($result);
 		mysql_select_db('stt', $g_link);
-	$blank="  ";
+		$blank="  ";
 
+	echo "<table border=0>";
 while($row = mysql_fetch_assoc($result))
         {	
-					$myid = $row['id'];
-					$myname = $row['username'];
-					$name = $row['name'];
-					$active = $row['active'];		
-					$idrow[$myid] = $myid;
-					$namerow[$myid] = $name;
-				print var_dump($namerow[$myid]);
-					$userrow[$myid] = $myname;
-					$activerow[$myid] = $active;
-        
-	
-$result = mysql_query($query);
-
-				echo "<table border=1>";
-				echo"<td></td>";
-				echo "<td>$namerow[$myid]</td>";
-	echo ("
-		<tr><td>Computer Software</td></tr>
-		<tr><td>Computer Hardware</td></tr>
-		<tr><td>Programming</td></tr>
-		<tr><td>Training/Documentation</td></tr>
-");
+	if($last != $row['name']){
+		echo "<tr><td>&nbsp;</td></tr>";
+		echo "<tr><td colspan=2>".$row['name']."</td></tr>";
+	}
+	echo "<tr><td>&nbsp;</td>";
+	echo "<td>".$row['skillName']."</td></tr>";
+	$last = $row['name'];
 		}
 
 
