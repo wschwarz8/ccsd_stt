60265850c2332929b5b8ed42ac25a6ed74527acc
maxdevos49 <maxdevos49@gmail.com>
2016-05-10 14:40:38 -0400

    finished creating a job from reward points page

--- awardpoints.php
+++ awardpoints.php
@@ -4,7 +4,7 @@ require_once 'functions.php';
 
 promptLogin(1);
 
-makeHeader("Award points", "Award Points", 2,"awardpoints.php", '<link href="/css_files/create_jobs.css" rel="stylesheet">');
+makeHeader("Award points", "Award Points", 2,"awardpoints.php", '<link href="/css_files/create_jobs.css" rel="stylesheet"><style>.meat{background:black;}</style>');
 
 $g_link = mysql_connect('localhost', $g_username, $g_password); //TODO use a persistant database connections
 
@@ -25,31 +25,68 @@ mysql_select_db('stt', $g_link);
 	<BODY>
 		
 <?php
+		
+
 $studentarray = array();
 $query = "SELECT name, id FROM students WHERE active=1";
 $result = mysql_query($query);
+		
+//give some variables that hold student info
 while ($row = mysql_fetch_assoc($result)) {
 	$studentarray[$row['id']]=$row['name'];
 }	
 
-if($_POST)
-{
+//process the form
+if($_POST){
+	
+	//remember the job id
 	$jobid=$_POST['job_id'];
+	
+	//check if a job needs resolved
 	if(isset($_POST['resolve'])){
+		
+		//make and do a query
 		$query = "UPDATE jobs SET status=4 WHERE id=".$jobid;
-   		$result = mysql_query($query);
-		if($result) {echo "resovled job ".$jobid.".<BR>";}
-		else {echo mysql_error($g_link);}
+   	$result = mysql_query($query);
+		
+		//check if the query was successful
+		if($result){
+			echo "resovled job ".$jobid.".<BR>";
+		}else{
+			echo mysql_error($g_link);
+		}
 	}
+	
+		if(isset($_POST['makeJobbox']) && $_POST['makeJobbox'] == 1){
+		
+		$makeJobQuery = "INSERT INTO `jobs`(`name`, `status`, `points`,`claimedby`) VALUES ('".$_POST['jobName']."',4,".$_POST['points'].",".$_POST['student_id'].")";
+		$result = mysql_query($makeJobQuery);
+		echo $jobid = mysql_insert_id();
+		if(!$result){
+			echo mysql_error($g_link);
+		}
+		
+	}
+	
+	//make points query and do it
 	$query = "INSERT INTO `stt`.`points` (`job_id`, `student_id`, `points`, `category_id`) VALUES ('".$jobid."', '".$_POST['student_id']."', '".$_POST['points']."', '".$_POST['category_id']."')";
-    $result = mysql_query($query);
-	if($result) {echo $_POST['points']." points added for student ".$studentarray[$_POST['student_id']].".<BR>";}
-	else {echo mysql_error($g_link);}
-}
-else
-{
+  $result = mysql_query($query);
+	
+	
+	//check if it was successful
+	if($result) {
+		echo $_POST['points']." points added for student ".$studentarray[$_POST['student_id']].".<BR>";
+	}else {
+		echo mysql_error($g_link);
+	}
+	
+}else{
+		//print this if no points were previously entered
     echo "Enter points here.<BR>";
 }
+
+		
+		
 ?>
 <form method=post name=theform>
 <table>
@@ -113,6 +150,7 @@ echo "
 <tr><td><input type=checkbox name="resolve" id="resolve" value="resolve">Resolve job?</td></tr>
 <tr><td>Points</td><td><input type="text" name="points" id="points"></td></tr>
 <tr><td>Point Category</td><td><input type="text" name="category_id" id="category_id" value="1"></td></tr>
+<tr><td>Create Job?<input name="makeJobbox" type='checkbox' value='1'></td><td>Job Name: <input name="jobName" type='text'></td></tr>
 <tr><td colspan=2><input type="submit"></td></tr>
 </table>
 </form>
